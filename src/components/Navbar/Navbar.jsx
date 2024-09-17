a9981717c50ef123b8b26b2b57ae42f0b400d88f
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-17 14:29:31 +0530

    Code Added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
-const Navbar = () => {
-  return <div>Navbar</div>;
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {motion} from 'framer-motion'
+const Navbar = ({sideBar,setSidebar}) => {
+  return <nav className="absolute top-0 left-0 w-full pt-10 text-white z-40">
+    <div className="container">
+      <div className="flex justify-between items-center">
+        {/* {logo section} */}
+        <motion.h1 
+        initial={{opacity:0 ,y:-100}} animate={{opacity:1 , y:0}}
+        transition={{type:"spring",stiffness:100,damping:10 , delay:0.2}}
+        className="text-2xl font-semibold uppercase"><span className="text-primary">Harsh-</span>Cafe</motion.h1>
+        {/* menu section} */}
+         <motion.div
+         initial={{opacity:0 ,y:-100}} animate={{opacity:1 , y:0}}
+         transition={{type:"spring",stiffness:100,damping:10 , delay:0.2}}
+         onClick={()=>setSidebar(!sideBar)}
+         >
+          <GiHamburgerMenu className="text-3xl cursor-pointer"/>
+         </motion.div>
+      </div>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
